35dfc5ad8b26f55834ab4f7d373672d459a84257
PSalador <salador2005@yandex.ru>
2019-05-26 13:33:21 +0000

    Fix

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Orchids\LMS\Providers;
+namespace :vendor\:package_name\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
