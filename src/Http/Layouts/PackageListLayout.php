b6c3947abb204d7398b079554ce346c2907414d2
PSalador <Salador2005@yandex.ru>
2020-08-27 19:15:18 +0700

    Update PackageListLayout.php

--- src/Http/Layouts/PackageListLayout.php
+++ src/Http/Layouts/PackageListLayout.php
@@ -1,6 +1,7 @@
 <?php
 namespace :vendor\:package_name\Http\Layouts;
 
+use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Layouts\Table;
 use Orchid\Screen\TD;
 
@@ -17,12 +18,15 @@ class :package_nameListLayout extends Table
     public function fields() : array
     {
         return  [
-			TD::set('input','Title')
-                ->link('platform.:_package_name.edit','id','title'),
-			TD::set('body', 'Text')
-                ->render(function ($data) {
-                    return str_limit($data->getContent('body'), 50);
-                }),
+		TD::set('input','Title')
+			->render(function ($data) {
+			    return Link::make($data->title)
+				->route('platform.:_package_name.edit', $data->id);
+			}),
+		TD::set('body', 'Text')
+			->render(function ($data) {
+			    return str_limit($data->getContent('body'), 50);
+			}),
         ];
     }
 }
