b515097c97b3a265fa5c7f5a0d1a05bc772cf1e0
PSalador <Salador2005@yandex.ru>
2020-08-27 19:34:45 +0700

    Update PackageListLayout.php

--- src/Http/Layouts/PackageListLayout.php
+++ src/Http/Layouts/PackageListLayout.php
@@ -1,6 +1,7 @@
 <?php
 namespace :vendor\:package_name\Http\Layouts;
 
+use Illuminate\Support\Str;
 use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Layouts\Table;
 use Orchid\Screen\TD;
@@ -18,14 +19,14 @@ class :package_nameListLayout extends Table
     public function columns() : array
     {
         return  [
-		TD::set('input','Title')
+		TD::set('title','Title')
 			->render(function ($data) {
-			    return Link::make($data->title)
+			    return Link::make($data->getContent('title'))
 				->route('platform.:_package_name.edit', $data->id);
 			}),
 		TD::set('body', 'Text')
 			->render(function ($data) {
-			    return str_limit($data->getContent('body'), 50);
+			    return Str::limit($data->getContent('body'), 50);
 			}),
         ];
     }
